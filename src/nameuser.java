eb138d22e22675f38ac62116ad8658ac11f57bce
ASUS <https://github.com/Sluckalex>
2022-02-20 20:56:48 +0300

    Enter name User

---
+++ src/nameuser.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class nameuser {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите имя User");
+        String user = sc.nextLine();
+        System.out.println("Hello " + user);
+        }
+    }
+
