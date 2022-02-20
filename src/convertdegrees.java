db582292860c74ca1d2ec3ac9eb2aef6f6a3e979
ASUS <https://github.com/Sluckalex>
2022-02-20 21:53:30 +0300

    convertdegrees

--- src/convertdegrees.java
+++ src/convertdegrees.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+
 public class convertdegrees {
     public static void main(String[] args) {
-        Scanner k = new Scanner(System.in);
-        System.out.println ("Введите количество градусов Цельсия: ");
-        int number1 = k.nextInt();
-        System.out.print((number1 * 1.8) + 32);
-        }
+        Scanner cels = new Scanner(System.in);
+        System.out.println("Введите количество градусов Цельсия:(целое число) ");
+        int number = cels.nextInt();
+        System.out.print("В "+ number + " градусов цельсия = " + ((number * 1.8) + 32) + " градусов по Фаренгейту ");
     }
+}
